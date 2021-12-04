3a74476c10ab85f19f34124f68dca070ede05a89
Sandulescu Dragos <47566133+sandulescudragos@users.noreply.github.com>
2021-12-04 21:01:38 +0200

    updated save method

--- search-api/src/start.ts
+++ search-api/src/start.ts
@@ -5,11 +5,16 @@ import { ElasticSearchService } from './service/elastic-search-service'
 (async () => {
 	dotenv.config();
 
-	let elasticSearchService = new ElasticSearchService('localhost', '9200', 'trace', '7.2');
+	let elasticSearchService = new ElasticSearchService('http://127.0.0.1', '9200', 'trace', '7.2');
 
 	await elasticSearchService.checkConnection();
 
 	const app = express();
+
+	elasticSearchService.save({
+		name: 'george',
+		date: Date.now()
+	});
 
 	app.listen(3000, () => console.log("Hakuna matata!"));
 
