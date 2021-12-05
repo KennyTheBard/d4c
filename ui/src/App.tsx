4d917ce09d4396949583a60e318d43486f5eeca8
Daniela ION <daniela.ion@stud.acs.upb.ro>
2021-12-05 03:18:14 +0200

    Ceva [mai]  bun

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Searchbar } from './components/Searchbar';
-import { Header } from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <br></br>
-        <Header/>
         <br></br>
         <Searchbar/>
         <br></br>
--- ui/src/components/Searchbar.tsx
+++ ui/src/components/Searchbar.tsx
@@ -2,6 +2,9 @@ import React, { Fragment } from "react";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export class SearchbarProp {}
@@ -15,19 +18,144 @@ export class Searchbar extends React.Component {
       this.state = {
         value: '',
         pageNumber :  1,
+        pageMaxEntries : 10,
         skip : 10,
         jobList : [
-          "purple",
-          "red",
-          "rewerd",
-          "rewred",
-          "rewrd",
-          "rewerd",
-          "rewerd",
-          "rew3rd",
-          "rewerd",
-          "rweed"
-        ]
+            {
+              source: "string",
+              jobId: "string",
+              jobIndex: 0,
+              link: "string",
+              title: "string",
+              company: "string",
+              place: "string",
+              date: "string",
+              description: "string",
+              senorityLevel: "string",
+              jobFunction: "string",
+              employmentType: "string",
+              industries: "string",
+            },
+            {
+              source: "string",
+              jobId: "string",
+              jobIndex: 0,
+              link: "string",
+              title: "string",
+              company: "string",
+              place: "string",
+              date: "string",
+              description: "string",
+              senorityLevel: "string",
+              jobFunction: "string",
+              employmentType: "string",
+              industries: "string",
+          },
+          {
+            source: "string",
+            jobId: "string",
+            jobIndex: 0,
+            link: "string",
+            title: "string",
+            company: "string",
+            place: "string",
+            date: "string",
+            description: "string",
+            senorityLevel: "string",
+            jobFunction: "string",
+            employmentType: "string",
+            industries: "string",
+          },
+          {
+            source: "string",
+            jobId: "string",
+            jobIndex: 0,
+            link: "string",
+            title: "string",
+            company: "string",
+            place: "string",
+            date: "string",
+            description: "string",
+            senorityLevel: "string",
+            jobFunction: "string",
+            employmentType: "string",
+            industries: "string",
+          },
+          {
+            source: "string",
+            jobId: "string",
+            jobIndex: 0,
+            link: "string",
+            title: "string",
+            company: "string",
+            place: "string",
+            date: "string",
+            description: "string",
+            senorityLevel: "string",
+            jobFunction: "string",
+            employmentType: "string",
+            industries: "string",
+          },
+          {
+            source: "string",
+            jobId: "string",
+            jobIndex: 0,
+            link: "string",
+            title: "string",
+            company: "string",
+            place: "string",
+            date: "string",
+            description: "string",
+            senorityLevel: "string",
+            jobFunction: "string",
+            employmentType: "string",
+            industries: "string",
+          },
+          {
+            source: "string",
+            jobId: "string",
+            jobIndex: 0,
+            link: "string",
+            title: "string",
+            company: "string",
+            place: "string",
+            date: "string",
+            description: "string",
+            senorityLevel: "string",
+            jobFunction: "string",
+            employmentType: "string",
+            industries: "string",
+          },
+          {
+            source: "string",
+            jobId: "string",
+            jobIndex: 0,
+            link: "string",
+            title: "string",
+            company: "string",
+            place: "string",
+            date: "string",
+            description: "string",
+            senorityLevel: "string",
+            jobFunction: "string",
+            employmentType: "string",
+            industries: "string",
+          },
+          {
+            source: "Google",
+            jobId: "string",
+            jobIndex: 0,
+            link: "https://www.google.com",
+            title: "tis tis tis",
+            company: "string",
+            place: "string",
+            date: "string",
+            description: "string",
+            senorityLevel: "string",
+            jobFunction: "string",
+            employmentType: "string",
+            industries: "string",
+        }]
     };
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
@@ -40,32 +168,31 @@ export class Searchbar extends React.Component {
     }
   
     async handleSubmit(event : React.SyntheticEvent) {
-      alert('You searched for a job which contain the following words: ' + this.state.value);
-      await this.setState({pageNumber: 1, skip: 10 });
+      // alert('You searched for a job which contain the following words: ' + this.state.value);
+      await this.setState({pageNumber: 1, skip: this.state.pageMaxEntries });
       event.preventDefault();
     }
 
     async handleClickPrev(event : React.SyntheticEvent) {
-      await this.setState({pageNumber: this.state.pageNumber - 1, skip: (this.state.pageNumber - 1) * 10 });
+      await this.setState({pageNumber: this.state.pageNumber - 1, skip: (this.state.pageNumber - 1) * this.state.pageMaxEntries });
       // alert('Ai mers inapoi la pagina: ' + this.state.pageNumber);
       event.preventDefault();
     }
 
     async handleClickNext(event : React.SyntheticEvent) {
-      await this.setState({pageNumber: this.state.pageNumber + 1, skip: (this.state.pageNumber + 1) * 10});
+      await this.setState({pageNumber: this.state.pageNumber + 1, skip: (this.state.pageNumber + 1) * this.state.pageMaxEntries});
       // alert('Ai mers mai departe la pagina: ' + this.state.pageNumber);
       event.preventDefault();
     }
   
-  
+    
     render() {
       return (
         <Fragment>
           
-            <InputGroup className="searchBar">
+            <InputGroup className="searchBar" size="lg">
 
               <Form.Control
-                className="searchBar"
                 placeholder="Engineer"
                 aria-label="SearchBar"
                 aria-describedby="basic-addon2"
@@ -78,29 +205,50 @@ export class Searchbar extends React.Component {
               </Button>
 
             </InputGroup>
-
-            <table>
+          
+            <div >
             {this.state.jobList.map((job:any, i:any) =>(
-              <tr key={i}>
-                {<td>
-                  {job}
-                </td>}
-              </tr>
-            ))}
-            </table>
+                <div className="cardsDetails" >
+              <Card>
+                <Card.Body>
+                  <Card.Title>{job.title}</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted employmentDetails">
+                    {job.company}
+                  </Card.Subtitle>
+                  <Card.Subtitle className="mb-2 text-muted employmentDetails">
+                    {job.industries}
+                  </Card.Subtitle>
+                  <Card.Subtitle className="mb-2 text-muted employmentDetails">
+                    {job.place}
+                  </Card.Subtitle>
 
+                  <Card.Text>
+                  {job.description}
+                  </Card.Text>
+
+                  <Card.Subtitle className="mb-2 text-muted employmentDetails" >
+                    {job.employmentType}
+                  </Card.Subtitle>
+                  <Card.Subtitle className="mb-2 text-muted employmentDetails" >
+                    {job.senorityLevel}
+                  </Card.Subtitle>
+                  <Button className="employmentDetails" variant="success"  href={job.link}>{job.source}</Button>
+                </Card.Body>
+              </Card>
+              </div>
+              ))}
+            </div>
+            
             <div className="pageNavigation">
-              <Button variant="outline-success" className="navigationButtons" onClick={this.handleClickPrev} disabled={this.state.pageNumber < 2}>
+              <Button variant="success" className="navigationButtons" onClick={this.handleClickPrev} disabled={this.state.pageNumber < 2}>
               PREV
               </Button>
 
-              <div  className="navigationButtons">
-              {this.state.pageNumber}
-              --
-              {this.state.skip}
+              <div className="pageNumber">
+                Page {this.state.pageNumber}
               </div>
 
-              <Button variant="outline-success" className="navigationButtons" onClick={this.handleClickNext}>
+              <Button variant="success" className="navigationButtons" onClick={this.handleClickNext} disabled={this.state.jobList.length  < this.state.pageMaxEntries}>
               NEXT
               </Button>
             </div>
